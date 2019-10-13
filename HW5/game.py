eb8636e671cfd5983f2c33a508ae42df3e16b039
tony <tony92151@gmail.com>
2019-10-14 00:38:17 +0800

    up game

---
+++ HW5/game.py
@@ -0,0 +1,68 @@
+import argparse 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation 
+import random
+import time
+
+class gameOfLive:
+	def __init__(self,size):
+		self.size = size
+		self.black = 0
+		self.white = 255
+		self.game = None
+		plt.ion()
+		self.fig, self.ax = plt.subplots() 
+
+	def initGame(self):
+		temp = []
+		for i in range(self.size**2):
+			if random.random()>0.5:
+				temp.append(self.white)	
+			else:
+				temp.append(self.black)
+		temp = np.array(temp).reshape((self.size,self.size))
+		self.game = temp
+		#plt.imshow(self.game)
+		#time.sleep(10)
+		#print(temp)
+		#print(self.game[2-1:2+2,:][:,2-1:2+2])
+
+	def step(self,frameNum):
+		tem = np.zeros(self.size**2).reshape((self.size,self.size))
+		for i in range(self.size):
+			for j in range(self.size):	
+
+				sum = (self.game[i-1:i+2,:][:,j-1:j+2].sum()-self.game[i][j])/255
+				#print("sum: ",sum)
+
+				if self.game[i][j] == self.white:	
+					if (sum < 2) or (sum > 3):
+						tem[i][j] = self.black
+					else:
+						tem[i][j] = self.white
+				elif self.game[i][j] == self.black:
+					if (sum == 3):
+						tem[i][j] = self.white
+					else:
+						tem[i][j] = self.black
+		self.game = tem
+		img = self.ax.imshow(a.game) 
+		#print("Game update!")
+	#def RunStepAnimation(self):
+		
+if __name__ == '__main__': 
+	a = gameOfLive(30)
+	a.initGame()
+
+	# ani = animation.FuncAnimation(a.fig, 
+	# 							a.step, 
+	# 							frames = 1, 
+	# 							interval=50)
+	# plt.show()
+	#
+	for i in range(1000):
+		a.step(0)
+		a.fig.canvas.draw()
+		a.fig.canvas.flush_events()
+		#time.sleep(0.05)
